d2b5171eca65cc8de9686f2ce64b312516ea459a
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2018-06-19 13:43:35 +0300

    added select,insert sort

---
+++ algo/sorting/src/select.js
@@ -0,0 +1,14 @@
+module.exports = {};
+
+var utils = require( './utils/utils')
+
+module.exports.sort = function(array){
+    for( var i = 0; i < array.length - 1; i++){
+        var minIndex = i;
+        for( var j = i + 1; j < array.length; j++){
+            if( array[ j] < array[ minIndex])
+                minIndex = j;
+        }
+        utils.swap( array, i, minIndex)
+    }
+}
--- algo/sorting/test/test.js
+++ algo/sorting/test/test.js
@@ -1,15 +1,22 @@
-var bubble = require( '../src/bubble')
 var config = require( './config')
 var utils = require( './utils/utils')
 
-console.time("script");
+var PATH_PREFIX = '../src/'
+var timeStamp = 'time'
 
-for( var i = 0; i < config.experimentsCount; i++){
-    var size = utils.randomInt( config.minSize, config.maxSize);
-    var array = utils.createArray( size, config.minValue, config.maxValue);
-    bubble.sort( array)
-    utils.checkSortIsCorrect( array);
+var sorters = [ 'bubble', 'select', 'insert']
+
+for( var i in sorters){
+    var sorter = require( PATH_PREFIX + sorters[ i])
+    console.log( sorters[ i] + ' sorting of ' + config.experimentsCount + ' arrays...')
+    console.time( timeStamp);
+    for( var i = 0; i < config.experimentsCount; i++){
+        var size = utils.randomInt( config.minSize, config.maxSize);
+        var array = utils.createArray( size, config.minValue, config.maxValue);
+        sorter.sort( array)
+        utils.checkSortIsCorrect( array);
+    }
+    console.log( 'OK');
+    console.timeEnd( timeStamp);
+    console.log();
 }
-
-console.log( "All experiments comleted successfully!");
-console.timeEnd("script");
