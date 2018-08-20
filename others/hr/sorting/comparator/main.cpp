476dccd099c4ad7c3c9b82d69bd973a3baf97ace
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2018-08-20 17:19:48 +0300

    minor fix

--- others/hr/sorting/comparator/main.cpp
+++ others/hr/sorting/comparator/main.cpp
@@ -2,12 +2,15 @@ class Checker{
 public:
   	// complete this method
     static int comparator(Player a, Player b)  {
-	   if( a.score == b.score){
+	    //The function comp returns true, when a should be found before b in the sorted array.
+        if( a.score == b.score){
            if( a.name < b.name)
                return 1;
            
            if( a.name > b.name)
                return -1;
+           
+           return 0;
        }
            
         if( a.score < b.score)
