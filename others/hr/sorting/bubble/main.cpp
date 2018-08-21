7536ba820af49e13b34660a95c4aae6f75b4d189
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2018-08-21 17:45:02 +0300

    prev

---
+++ others/hr/sorting/bubble/main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countSwaps function below.
+void countSwaps(vector<int> a) {
+    int res = 0;
+    for( int i = 0; i < a.size() - 1; i++){
+        for( int j = i; j < a.size(); j++){
+            if( a[ i] > a[ j]){
+                int temp = a[ i];
+                a[ i] = a[ j];
+                a[ j] = temp;
+                res++;
+            }
+        }
+    }
+    
+    std::cout << "Array is sorted in " << res << " swaps." << std::endl;
+    std::cout << "First Element: " << a[0] << std::endl;
+    std::cout << "Last Element: " << a[a.size() - 1] << std::endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
