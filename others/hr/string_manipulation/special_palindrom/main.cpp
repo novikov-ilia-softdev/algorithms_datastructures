bad6b2f2287a3355e6125ce533e8b1f29bdb15a9
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2018-09-03 14:42:36 +0300

    string_manipulation/special_palindrom

--- others/hr/string_manipulation/special_palindrom/main.cpp
+++ others/hr/string_manipulation/special_palindrom/main.cpp
@@ -2,71 +2,34 @@
 
 using namespace std;
 
-void print( const map<string, bool> m){
-    cout << "cache" << endl;
-    for( auto it = m.begin(); it != m.end(); it++){
-        cout << it->first << ": " << it->second << endl;
-    }
-    
-}
-
-bool isSpecialPalindrom( const std::string& s){
-    //cout << "isSpecialPalindrom: " << s << ", ";
-    static map<string, bool> isStrSpecPalindromMap;
-    
-    auto it = isStrSpecPalindromMap.find( s);
-    if( it != isStrSpecPalindromMap.end()){
-        //cout << "from cache " << it->second << endl;
-        return it->second;
-    }
-        
-    if( s.length() == 1){
-        //cout << "true" << endl;
-        isStrSpecPalindromMap.insert( pair<string,bool>(s, true));
-        //print( isStrSpecPalindromMap);
-        return true;
-    }
-        
-    bool isLengthEven = s.length() % 2 == 0;
-    int medIndex = isLengthEven ? -1 : s.length() / 2;
-    
-    char firstCh = s[0];
-    for(int i = 1; i < s.length(); i++){
-        if( i == medIndex)
-            continue;
-
-        if( s[i] != firstCh){
-            //cout << "false" << endl;
-            isStrSpecPalindromMap.insert( pair<string,bool>(s, false));
-            //print( isStrSpecPalindromMap);
-            return false;
-        }
-            
-    }
-    
-    //cout << "true" << endl;
-    isStrSpecPalindromMap.insert( pair<string,bool>(s, true));
-    //print( isStrSpecPalindromMap);
-    return true;
-}
-
-void recursiveCount( const std::string& s, long& res){
-    //cout << endl << "recursiveCount: " << s << endl;
-    
-    if( s == "")
-        return;
-    
-    for(int i = 1; i <= s.length(); i++){
-        if( isSpecialPalindrom( s.substr(0, i)))
-            res++;
-    }
-    
-    recursiveCount( s.substr( 1), res);
-}
-
 long substrCount(int n, string s) {
     long res = 0;
-    recursiveCount( s, res);
+    
+    for( int i = 0; i < s.length(); i++){
+        res++;
+            
+        if( i == s.length() - 1)
+            continue;
+        
+        char ch = s[i + 1];
+        
+        // odd
+        int leftInd = i;
+        int rightInd = i;
+        while( --leftInd != -1 && ++rightInd != s.length() &&
+               s[leftInd] == ch && s[rightInd] == ch){
+            res++;
+        }
+        
+        // even
+        rightInd = i;
+        while( ++rightInd != s.length() && s[rightInd] == s[i]){
+            if( (rightInd - i) % 2 == 1){
+                res++;
+            }
+        }
+    }
+    
     return res;
 }
 
