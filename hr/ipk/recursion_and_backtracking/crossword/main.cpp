82730b49c71eabfb8a940adb245da6167e2dd3ea
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2019-04-09 18:01:48 +0300

    recursion_and_backtracking/crossword

--- hr/ipk/recursion_and_backtracking/crossword/main.cpp
+++ hr/ipk/recursion_and_backtracking/crossword/main.cpp
@@ -3,20 +3,29 @@
 using namespace std;
 
 struct Point{
+  Point( int i_, int j_): i( i_), j( j_) {}
   int i;
   int j;
 };
 
 struct Gap{
+    Gap( Point start_, Point end_): start( start_), end( end_) {}
     Point start;
     Point end;
-    int length;
+    
+    bool isHorizontal() const { return start.i == end.i; }
+    int getLength() const {
+        if( isHorizontal())
+            return end.j - start.j + 1;
+        
+        return end.i - start.i + 1;
+    }
 };
 
 typedef vector<string> Crossword;
 typedef vector<Gap> Gaps;
 typedef set<string> Visited;
-
+typedef vector<string> Words;
 
 string createKey( int i, int j){
     return to_string( i) + "_" + to_string( j);
@@ -55,15 +64,26 @@ Gaps getGaps( const Crossword& crossword){
                 if( visitedRight.find( createKey( i, j)) == visitedRight.end()){
                     visitedRight.insert( createKey( i, j));
                     int right = moveRight( i, j, crossword, visitedRight);
-                    if( right != j)
-                        cout << i << "," << j << " -> " << i << "," << right << endl;
+                    if( right != j){
+                        Point start( i, j);
+                        Point end( i, right);
+                        Gap gap( start, end);
+                        gaps.push_back( gap);
+                    }
+                        
                 }
                 
                 if( visitedDown.find( createKey( i, j)) == visitedDown.end()){
                     visitedDown.insert( createKey( i, j));
                     int down = moveDown( i, j, crossword, visitedDown);
-                    if( down != i)
-                        cout << i << "," << j << " -> " << down << "," << j << endl;
+                    if( down != i){
+                        Point start( i, j);
+                        Point end( down, j);
+                        Gap gap( start, end);
+                        gaps.push_back( gap);
+                        
+                    }
+                        
                 }
                 
             }
@@ -73,41 +93,134 @@ Gaps getGaps( const Crossword& crossword){
     return gaps;
 }
 
+class DebugUtils{
+public:
+    static void printGaps( const Gaps& gaps){
+        for( auto& gap : gaps){
+            cout << gap.start.i << "," << gap.start.j << " -> " << gap.end.i << "," << gap.end.j << " (" << gap.getLength() << ")" << endl;
+        }
+    }
+    
+    static void printWords( const Words& words){
+        for( auto& word : words){
+            cout << word << endl;
+        }
+    }
+    
+    static void printCrossword( const Crossword& crossword){
+        for( auto& line : crossword){
+            cout << line << endl;
+        }
+        cout << endl;
+    }
+};
+
 /*
 isWordSuitsGap
 deleteGap
 deleteWord
+*/
 
-void solveCrosswordRecursive( solvedCrossword, gaps, wordCandidate, words){
+string getGapValue( Gap gap, const Crossword& solvedCrossword){
+    string res = "";
+    
+    if( gap.isHorizontal()){
+        for( int i = gap.start.j; i <= gap.end.j; i++){
+            res += solvedCrossword[ gap.start.i][ i];
+        }
+    }
+    else{
+        for( int i = gap.start.i; i <= gap.end.i; i++){
+            res += solvedCrossword[ i][ gap.start.j];
+        }
+    }
+    
+    return res;
+}
+
+bool isWordSuitsGap( string word, Gap gap, const Crossword& solvedCrossword){
+    //cout << "isWordSuitsGap" << endl;
+    //cout << "word: " << word << endl;
+    
+    if( word.size() != gap.getLength())
+        return false;
+    
+    string gapValue = getGapValue( gap, solvedCrossword);
+    //cout << "gapValue: " << gapValue << endl;
+    
+    for( int i = 0; i < word.size(); i++){
+        if( gapValue[ i] != '-' && gapValue[ i] != word[ i])
+            return false;
+    }
+    
+    return true;
+}
+
+void fillGap( Crossword& solvedCrossword, Gap gap, string word){
+    if( gap.isHorizontal()){
+        for( int i = gap.start.j, j = 0; i <= gap.end.j; i++, j++){
+            solvedCrossword[ gap.start.i][i] = word[ j];
+        }
+    }
+    else{
+        for( int i = gap.start.i, j = 0; i <= gap.end.i; i++, j++){
+            solvedCrossword[ i][gap.start.j] = word[ j];
+        }
+    }
+}
+
+bool solveCrosswordRecursive( Crossword& solvedCrossword, Gaps gaps, string wordCandidate, Words words){
+    
+    //cout << "solveCrosswordRecursive" << endl;
+    //cout << "gaps.size(): " << gaps.size() << endl;
+    //cout << "wordCandidate: " << wordCandidate << endl;
+    
     if( !gaps.size())
 	return true;
     
-    if( isWordSuitsGap( wordCandidate, gaps[ 0])){
-        deleteGap( gaps[ 0]);
-        deleteWord( wordCandidate, words);
+    if( isWordSuitsGap( wordCandidate, gaps[ 0], solvedCrossword)){
+        fillGap( solvedCrossword, gaps[ 0], wordCandidate);
+        gaps.erase( gaps.begin());
+        auto it = find(words.begin(), words.end(), wordCandidate);
+        if (it != words.end()) words.erase(it);
     }
     
-    for( auto word in words){
-	return solveCrosswordRecursive( solvedCrossword, gaps, word, words)
+    DebugUtils::printCrossword( solvedCrossword);
+    
+    /*
+    for( auto& word : words){
+	if( solveCrosswordRecursive( solvedCrossword, gaps, word, words))
+            return true;
     }
+    */
     
     return false;
 }
-*/
+
+
+Words split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    Words elems;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    
+    return elems;
+}
 
 vector<string> crosswordPuzzle(vector<string> crossword, string words) {
     Gaps gaps = getGaps( crossword);
+    DebugUtils::printGaps( gaps);
     
+    Words splittedWords = split( words, ';');
+    DebugUtils::printWords( splittedWords);
     
-    //DebugUtils::printGaps( gaps);
-    
-    /*
-    for( auto word in words){
-	vector<string> solvedCrossword = crossword;
-	if( solveCrosswordRecursive( solvedCrossword, gaps, word, words))
+    for( auto& word : splittedWords){
+	Crossword solvedCrossword = crossword;
+	if( solveCrosswordRecursive( solvedCrossword, gaps, word, splittedWords))
 	    return solvedCrossword;
     }
-    */
     
     return crossword;
 }
