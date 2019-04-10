7f95e549665218951f8ae59bf8dc76ab3a80237c
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2019-04-10 16:51:34 +0300

    misc/max_xor

---
+++ hr/ipk/misc/max_xor/main.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Brute force
+vector<int> maxXor(vector<int> arr, vector<int> queries) {
+    vector<int> res;
+    for( int i = 0; i < queries.size(); i++){
+        int maxXor = queries[ i] ^ arr[0];
+        
+        for( int j = 1; j < arr.size(); j++){
+            int curXor = queries[ i] ^ arr[j];
+            if( curXor > maxXor)
+                maxXor = curXor;
+        }
+        
+        res.push_back( maxXor);
+    }
+    
+    return res;
+}
+
+// TODO
+// create trie
+// make search of inversion
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int m;
+    cin >> m;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> queries(m);
+
+    for (int i = 0; i < m; i++) {
+        int queries_item;
+        cin >> queries_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> result = maxXor(arr, queries);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
