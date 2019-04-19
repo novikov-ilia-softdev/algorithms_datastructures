2e33df3b07f135d7dc706792d7dbf03a494aa62e
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2019-04-19 14:04:11 +0300

    dynamic_programming/abbreviation

--- hr/ipk/dynamic_programming/abbreviation/main.cpp
+++ hr/ipk/dynamic_programming/abbreviation/main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/*
 string abbreviation(string a, string b) {
     
     //cout << endl << "a: " << a << ", b:" << b << endl;
@@ -48,11 +48,71 @@ string abbreviation(string a, string b) {
     cout << "final NO " << endl;
     return "NO";
 }
+*/
+
+bool isPossibleToTransform(string a, string b){
+    cout << "a: " << a << ", b:" << b << endl;
+    cin.get();
+    
+    if( a == "" && b == ""){
+        cout << "true" << endl;
+        return true;
+    }
+        
+    if( a == ""){
+        cout << "false (a empty)" << endl;
+        return false;
+    }
+        
+    if( b == ""){
+        if( isupper( a[0])){
+            cout << "false (b empty, a[0] is upper)" << endl;
+            return false;
+        }
+            
+        
+        return isPossibleToTransform( a.substr( 1), b);
+    }
+        
+    if( islower( a[0])){
+        a[ 0] = toupper( a[0]);
+            if( isPossibleToTransform( a, b)){
+                cout << "false (a[0] up)" << endl;
+                return true;
+            }
+                
+        if( isPossibleToTransform( a.substr( 1), b)){
+            cout << "false (a[0] delete)" << endl;
+            return true;
+        }
+    }
+    
+    if( a[ 0] == b[ 0])
+        return isPossibleToTransform( a.substr( 1), b.substr( 1));
+    else{
+        cout << "false (a[ 0] != b[ 0])" << endl;
+        return false;
+    }
+    
+    //cout << "false (no way)" << endl;
+    return false;
+}
+
+string abbreviation(string a, string b) {
+    
+    if( isPossibleToTransform( a, b)){
+        cout << "YES" << endl;
+        return "YES";
+    }
+        
+    cout << "NO" << endl;
+    return "NO";
+}
 
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
-    ifstream fin("input06.txt");
+    ifstream fin("input12_2.txt");
 
     int q;
     fin >> q;
