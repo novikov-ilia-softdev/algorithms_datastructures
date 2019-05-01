ef5bb94638d9cf3e25204e3d4faff2b15755e9b2
smartnlgilianovikov <smartnlg.ilia.novikov@gmail.com>
2019-05-01 03:54:40 +0300

    dynamic_programming/abbreviation

--- hr/ipk/dynamic_programming/abbreviation/main.cpp
+++ hr/ipk/dynamic_programming/abbreviation/main.cpp
@@ -50,51 +50,43 @@ string abbreviation(string a, string b) {
 }
 */
 
+bool isEmpty( const string& s){
+    return s.size() == 0;
+}
+
 bool isPossibleToTransform(string a, string b){
-    cout << "a: " << a << ", b:" << b << endl;
-    cin.get();
+    //cout << "a: " << a << endl << "b: " << b << endl;
+    //cout << "reason: " << reason << endl;
+    //cin.get();
     
-    if( a == "" && b == ""){
-        cout << "true" << endl;
+    if( isEmpty( a) && isEmpty( b))
         return true;
-    }
         
-    if( a == ""){
-        cout << "false (a empty)" << endl;
+    if( isEmpty( a) && !isEmpty( b))
         return false;
-    }
         
-    if( b == ""){
-        if( isupper( a[0])){
-            cout << "false (b empty, a[0] is upper)" << endl;
+    if( !isEmpty( a) && isEmpty( b)){
+        if( isupper( a[0]))
             return false;
-        }
             
-        
         return isPossibleToTransform( a.substr( 1), b);
     }
         
     if( islower( a[0])){
         a[ 0] = toupper( a[0]);
-            if( isPossibleToTransform( a, b)){
-                cout << "false (a[0] up)" << endl;
-                return true;
-            }
+
+	if( isPossibleToTransform( a, b))
+	    return true;
                 
-        if( isPossibleToTransform( a.substr( 1), b)){
-            cout << "false (a[0] delete)" << endl;
+        if( isPossibleToTransform( a.substr( 1), b))
             return true;
-        }
+        
+        return false;
     }
     
     if( a[ 0] == b[ 0])
         return isPossibleToTransform( a.substr( 1), b.substr( 1));
-    else{
-        cout << "false (a[ 0] != b[ 0])" << endl;
-        return false;
-    }
-    
-    //cout << "false (no way)" << endl;
+
     return false;
 }
 
@@ -112,8 +104,8 @@ string abbreviation(string a, string b) {
 int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
-    ifstream fin("input12_2.txt");
-
+    ifstream fin("input12_3.txt");
+    
     int q;
     fin >> q;
     fin.ignore(numeric_limits<streamsize>::max(), '\n');
