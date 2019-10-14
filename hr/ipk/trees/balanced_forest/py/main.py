b3f5bafb66d9c222b7126a65721a1541d5bff3fc
Ilia Novikov <smartnlg.ilia.novikov@gmail.com>
2019-10-14 10:24:55 +0300

    ipk/trees/balanced_forest

--- hr/ipk/trees/balanced_forest/py/main.py
+++ hr/ipk/trees/balanced_forest/py/main.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+import queue
 
 def delete_edges(edges, i, j):
     result = []
@@ -17,20 +18,50 @@ def delete_edges(edges, i, j):
     return result
 
 def get_forest(nodes, edges):
-    print('get_forest')
-    print(nodes)
-    print(edges)
-    dict = {}
+    #print('get_forest')
+    #print(nodes)
+    #print(edges)
+    dictionary = {}
 
     for i in range(1,len(nodes) + 1):
-        dict[i] = []
+        dictionary[i] = {}
+        dictionary[i]['with'] = []
+        dictionary[i]['val'] = nodes[i - 1]
+        dictionary[i]['visited'] = False
 
     for edge in edges:
-        dict[edge[0]].append(edge[1])
-        dict[edge[1]].append(edge[0])
+        dictionary[edge[0]]['with'].append(edge[1])
+        dictionary[edge[1]]['with'].append(edge[0])
     
-    print( dict)
-    print()
+    #print( dictionary)
+   
+    forest_sums = []
+
+    for node in dictionary:
+        #print( node)
+        if( dictionary[node]['visited'] == False):
+            treeSum = 0
+            q = queue.Queue()
+            q.put(node)
+            while not q.empty():
+                cur = q.get()
+                treeSum = treeSum + dictionary[cur]['val']
+                dictionary[cur]['visited'] = True
+                for i in dictionary[cur]['with']:
+                    if( dictionary[i]['visited'] == False):
+                        q.put(i)
+
+            forest_sums.append( treeSum)
+ 
+    forest_sums.sort()
+    #print(forest_sums)
+
+    if( len(forest_sums) == 3 and forest_sums[1] == forest_sums[2]):
+        return forest_sums[1] - forest_sums[0]
+    else:
+        return -1
+    
+    #print()
 
 class Tree:
     def __init__( self, nodes, edges):
@@ -43,11 +74,13 @@ class Tree:
             for j in range( i + 1, len(self._edges)):
                 #print( i, j)
                 new_edges = delete_edges(self._edges, i, j)
-                print( new_edges)
-                forest = get_forest( self._nodes, new_edges)
+                #print( new_edges)
+                new_node = get_forest( self._nodes, new_edges)
+                #print( new_node)
                 #new_node = self._get_new_node( forest)
-                #if(new_node > 0 and new_node < min_node):
-                #    min_node = new_node
+                if(new_node != -1):
+                    if(min_node == -1 or new_node < min_node):
+                        min_node = new_node
 
         return min_node
 
@@ -55,8 +88,8 @@ class Tree:
 #[1, 2, 2, 1, 1]
 #[[1, 2], [1, 3], [3, 5], [1, 4]]
 def balancedForest(c, edges):
-    print( c)
-    print( edges)
+    #print( c)
+    #print( edges)
     tree = Tree( c, edges)
     return tree.get_balanced_forest()
 
